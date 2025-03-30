708190c5317dc1f239fd0f1b79291010cdf24b36
GianCode888 <liberakylegian35@gmail.com>
2025-03-30 22:05:22 +0800

    Database Connection File

---
+++ database_management.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db_name = "account_management";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
